7750111b5119da1ae3ce63afe37597351a79e8e4
Artem <artemzhdanov1299@gmail.com>
2022-10-24 11:31:15 +0300

    Add project files.

---
+++ Exam_project/info.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <Windows.h> 
+#include "Maxsum_in_month.h"
+#include "Mono_card.h"
+#include "privat_card.h"
+#include "resourse_category.h"
+#include "Save_info.h"
+#include "Statistic.h"
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+
+}
